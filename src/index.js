4412dd8d7a8f2fc545761e68ec10e60076410a7a
KapitanPiratu <janek.marek@outlook.cz>
2023-05-29 19:27:33 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const SECRET = '1234' //testing value
+
+app.get('/', (req, res) => {
+    const page = req.query['page'];
+    let path;
+    console.log(page)
+    switch (page) {
+        case 'main':
+            path = '/public/main.html';
+            break;
+        case 'secondary':
+            path = '/public/secondary.html';
+            break;
+        case 'alert':
+            path = '/public/alert.html';
+            break;
+        case 'admin':
+            path = '/public/admin.html';
+            break;
+        default:
+            path = '/public/index.html';
+            break;
+    }
+    res.sendFile(__dirname + path);
+});
+
+//handling sockets
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.emit('console_msg', { msg: `a device connected (IP: "${socket.handshake.address}")`});
+    
+    socket.on('check_answer', ans => {
+        if (ans == SECRET){
+            io.emit('console_msg', { msg: `Answer was submitted: "${ans}" - CORRECT`, correct: true })
+        } else{
+            io.emit('console_msg', { msg: `Answer was submitted: "${ans}" - WRONG`, correct: false })
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+        io.emit('console_msg', { msg: `device disconnected (IP: "${socket.handshake.address}")`});
+    });
+});
+
+server.listen(3000, () => {
+  console.log('listening on port 3000');
+});
